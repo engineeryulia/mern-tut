0d32cb79b0d891d583628ac887eed31ae818a9e1
engeneeryulia@gmail.com <engineeryulia@gmail.com>
2024-05-24 20:19:40 -0600

    Create goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,12 @@
 const asyncHandler = require('express-async-handler')
+
+const Goal = require('../model/goalModel')
 //@desc  Get goals
 //rout GET/api/goals
 //access Private
 const getGoals = asyncHandler(async(req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 //@desc  Set goals
@@ -14,7 +17,11 @@ const setGoal = asyncHandler(async(req, res) => {
         res.status(400)//.json({message: 'Please add a text field'})
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message: 'Set goals'})
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 })
 
 //@desc  Update goals
